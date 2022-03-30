21e88d464c1789ac3959c0d67895abe2051af445
luis-camacho-dev <luis.c.c.dev@gmail.com>
2022-03-30 00:52:04 -0400

    Add Swagger documentation

--- ServiceRequest/Program.cs
+++ ServiceRequest/Program.cs
@@ -4,21 +4,32 @@ using Data;
 using ILogic;
 using BusinessLogic;
 using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers().AddJsonOptions( x => x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ServiceRequestContext>(opt => opt.UseInMemoryDatabase("ServiceRequest"));
 
 builder.Services.AddTransient<UnitOfWork, UnitOfWork>();
 builder.Services.AddTransient<IServiceRequestManager, ServiceRequestManager>();
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+
+
 
 app.UseAuthorization();
 
